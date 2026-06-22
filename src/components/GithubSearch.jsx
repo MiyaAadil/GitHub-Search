6cb2233a1de91547c91e7c027648baf9b7810457
MiyaAadil <miyaaadil76@gmail.com>
2026-06-22 12:58:58 +0545

    change error color to red

--- src/components/GithubSearch.jsx
+++ src/components/GithubSearch.jsx
@@ -40,7 +40,7 @@ const GitHubSearch = () => {
       <button className="bg-blue-900 hover:bg-blue-950 cursor-pointer text-white w-30 p-2 rounded-3xl mx-auto active:scale-95" onClick={fetchUser}>Search</button>
 
           {loading && <p className="text-3xl text-gray-400 text-center">Loading...</p>}
-          {error && <p className="text-3xl text-gray-400 text-center">{error}</p>}
+          {error && <p className="text-3xl text-red-400 text-center">{error}</p>}
           {userdata && (
             <div className="flex flex-col gap-1 justify-center items-center border-2 p-2 rounded-xl">
               <img className="h-50 w-50 rounded-md bg-gray-100" src={userdata.avatar_url} alt={userdata.name} width={100} />
